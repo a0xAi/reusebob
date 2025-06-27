35f339a185a727262dc765c9f774adb952778871
Adam Malewski <adam.malevski@ovoko.com>
2025-06-27 16:54:08 +0300

    messages intro

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { useFocusEffect } from 'expo-router';
 import { useCallback } from 'react';
 import { useSession } from '../../ctx';
@@ -7,6 +7,7 @@ import { getUserByID } from '@/database/user';
 import { getAllChats } from '@/database/chat';
 import RichMenuItem from '@/components/RichMenuItem';
 import MenuItem from '@/components/MenuItem';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function MessagesScreen() {
   const [chats, setChats] = useState([]);
@@ -14,22 +15,43 @@ export default function MessagesScreen() {
   useFocusEffect(
     useCallback(() => {
       (async () => {
+        // console.log(111)
         const all = await getAllChats();
+        // console.log(all)
         setChats(all);
       })();
 
       return () => {
-        // console.log("This route is now unfocused.");
+        console.log("messages route is now unfocused.");
       };
     }, [])
   );
   return (
-    <View style={styles.container}>
-      <Text>Zinutes cia</Text>
-      <Text>
+    <ScrollView style={styles.container}>
+      {/* <Text>
         {JSON.stringify(chats)}
-      </Text>
-    </View>
+      </Text> */}
+      <View>
+        {chats.map((chat, key) => (
+          <TouchableOpacity
+            style={{
+              borderBottomWidth: 1,
+              borderBottomColor: '#ddd',
+              padding: 16,
+              flexDirection: 'row',
+            }}
+          >
+            <View style={{ backgroundColor: '#555', width: 48, height: 48, borderRadius: 24, alignItems: 'center', justifyContent: 'center' }}>
+              <MaterialIcons name="chat" size={24} color='#fff' />
+            </View>
+            <View style={{ justifyContent: 'center', marginLeft: 8, }}>
+              <Text style={{ fontFamily: 'Inter_700Bold' }}>{`${chat.buyer.name} - ${chat.listing.name}`}</Text>
+              <Text style={{ fontFamily: 'Inter_500Medium' }}>last message</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
--- database/chat.js
+++ database/chat.js
@@ -1,17 +1,40 @@
 import app from '../firebaseConfig';
-import { getFirestore, getDoc, setDoc, doc, } from "firebase/firestore";
+import { getFirestore, getDoc, setDoc, doc, docs, collection, getDocs, } from "firebase/firestore";
 
 const db = getFirestore(app);
 
 export const getAllChats = async () => {
-  const docRef = doc(db, 'chats', id);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    return { id: docSnap.id, ...docSnap.data() };
-  } else {
-    console.log("No such document!");
-    return null;
+  const chatsCollectionRef = collection(db, 'chats');
+  const chatsSnap = await getDocs(chatsCollectionRef);
+  const chats = [];
+  for (const docSnap of chatsSnap.docs) {
+    const id = docSnap.id;
+    const data = docSnap.data();
+    let seller = null;
+    if (data.sellerRef) {
+      const sellerSnap = await getDoc(data.sellerRef);
+      if (sellerSnap.exists()) {
+        seller = { id: sellerSnap.id, ...sellerSnap.data() };
+      }
+    }
+    let buyer = null;
+    if (data.buyerRef) {
+      const buyerSnap = await getDoc(data.buyerRef);
+      if (buyerSnap.exists()) {
+        buyer = { id: buyerSnap.id, ...buyerSnap.data() };
+      }
+    }
+    let listing = null;
+    if (data.listingRef) {
+      const listingSnap = await getDoc(data.listingRef);
+      if (listingSnap.exists()) {
+        listing = { id: listingSnap.id, ...listingSnap.data() };
+      }
+    }
+    console.log({ id, ...data, seller, buyer, listing })
+    chats.push({ id, ...data, seller, buyer, listing });
   }
+  return chats;
 }
 
 
